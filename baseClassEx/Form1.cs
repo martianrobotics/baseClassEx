9e8d2f69b4df0c5e6edc4727461aac22635b2a25
martianrobotics <jupiter.arias@gmail.com>
2017-07-05 13:13:05 -0400

    Add project files.

---
+++ baseClassEx/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace baseClassEx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyBaseClass mbc = new MyBaseClass("baseClass Needs This!");
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySubclass msc = new MySubclass("subclass needs this!", 88);
+        }
+    }
+}
---
+++ baseClassEx/MyBaseClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace baseClassEx
+{
+    class MyBaseClass
+    {
+        public MyBaseClass(string baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the base class: " + baseClassNeedsThis);
+        }
+    }
+
+    class MySubclass : MyBaseClass
+    {
+        public MySubclass(string baseClassNeedsThis, int anotherValue)
+            : base(baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the subclass: " + baseClassNeedsThis + " and " + anotherValue);
+        }
+    }
+
+}
